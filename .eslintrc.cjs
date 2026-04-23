b76580218af092fafc5c10517f72f3304497cb34
Felix Serang <byte007@zoho.com>
2026-04-23 21:08:14 +0700

    chore: initialize pnpm monorepo with shared toolchain

---
+++ .eslintrc.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 2022, sourceType: 'module' },
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  env: { node: true, es2022: true },
+  ignorePatterns: ['dist', 'build', 'node_modules', 'components/**', 'styles.css', 'index.html'],
+  rules: {
+    '@typescript-eslint/consistent-type-imports': 'warn',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+    ],
+  },
+};
